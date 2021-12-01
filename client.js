6e4f0991f4685bc0c291dbc1eeb87a5bd554df54
James <jameshuang337@gmail.com>
2021-12-01 15:14:34 -0800

    moving connection logic to its own module

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+    const conn = net.createConnection({
+        host: '165.227.47.243',
+        port: 50541
+    });
+
+    conn.on('connect', () => {
+        console.log('connected')
+    })
+
+    conn.on('data', (data) => {
+        console.log(data)
+    });
+
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+
+    return conn;
+};
+
+connect();
+
+module.exports = {
+    connect
+};
