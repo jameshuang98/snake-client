f927dffd9d73808f64f430628950b61ee84935d6
James <jameshuang337@gmail.com>
2021-12-01 16:26:54 -0800

    updated modules to allow for wasd movement

--- input.js
+++ input.js
@@ -1,7 +1,16 @@
- 
+w = "Move: up"
+a = "Move: left"
+s = "Move: down"
+d = "Move: right"
+
+// Stores the active TCP connection object.
+let connection;
+
+
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = (conn) => {
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
@@ -13,6 +22,14 @@ const setupInput = function () {
 const handleUserInput = function(key) {
     if (key === '\u0003') {
         process.exit();
+    } else if (key === 'w') {
+        connection.write(w)
+    } else if (key === 'a') {
+        connection.write(a)
+    } else if (key === 's') {
+        connection.write(s)
+    } else if (key === 'd') {
+        connection.write(d)
     }
 };
 
